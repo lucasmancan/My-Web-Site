addabf0a7a114e8708a9d2638293c5fcd2f95a3a
Lucas Mançan <lucasfmancan@hotmail.com>
2018-02-03 14:45:55 -0200

    final

---
+++ index.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+	<title>Lucas Mançan | Site</title>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb"
+	 crossorigin="anonymous">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/style-timeline.css">
+	<link rel="icon" href="imgs/favicon.ico">
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+</head>
+
+<body>
+	<div id="particles-js"></div>
+	<script src="js/particles.min.js"></script>
+	<script src="js/app.js"></script>
+	<section class="capa">
+		<div class="texto-capa ">
+			<div class=" caixa">
+				<!-- <h1 id="nome-perfil">Lucas Frederico Mançan</h1>-->
+				<h1>
+					<span id="typed" class="element"></span>
+				</h1>
+				<div class="rede">
+					<a href="https://www.facebook.com/lucas.fredericomancan" target="_blank">
+						<img class="icone" src="imgs/facebook.png" alt="facebook">
+					</a>
+					<a href="https://github.com/lucasmancan" target="_blank">
+						<img class="icone" src="imgs/GitHub.png" alt="GitHub">
+					</a>
+					<a href="https://www.instagram.com/lucas_mancan/" target="_blank">
+						<img class="icone" src="imgs/instagram.png" alt="GitHub">
+					</a>
+				</div>
+				<a class="btn btn-principal" href="#sobre">Saiba Mais!</a>
+			</div>
+		</div>
+		<div id="mensagem-link">
+			<a id="btn-contato" href="#contato">
+				<img class="mensagem-icon" src="imgs/mensagem.png" alt="mensagem">
+			</a>
+		</div>
+	</section>
+	</div>
+	<section id="sobre">
+		<div class="container">
+			<div class="row ">
+				<div class="col-md-8 anime ">
+					<h1 id="sobrep">Sobre Mim</h1>
+					<p>Tenho 19 anos, sou de Vargem Grande Paulista, uma cidade da região metropolitana de São Paulo. Sempre fui ligado a tecnologia
+						e mesmo durante a escola já me aventurava no ramo de desenvolvimento.
+					</p>
+					<a class=" btn btn-black" href="#cd-timeline">Conheça minhas formações</a>
+				</div>
+				<div class="col-md-4 img-container">
+					<img id="img-perfil" src="imgs/perfil.jpg">
+				</div>
+				<div class="row">
+				</div>
+			</div>
+		</div>
+	</section>
+	<section id="cd-timeline" class="cd-container">
+		<div class="cd-timeline-block ">
+			<div class="cd-timeline-img cd-movie">
+				<img src="imgs/cd-icon-location.svg" alt="Picture">
+			</div>
+			<!-- cd-timeline-img -->
+			<div class="cd-timeline-content">
+				<h2>Ensino médio</h2>
+				<img class="img-timeline" src="imgs/logoObjetivo.png">
+				<p>Colégio Objetivo Vargem Grande Paulista</p>
+				<a href="https://www.objetivovgp.com/" target="_blank" class="cd-read-more">Conheça a Escola</a>
+				<span class="cd-date">Dezembro 2015</span>
+			</div>
+			<!-- cd-timeline-content -->
+		</div>
+		<!-- cd-timeline-block -->
+		<div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-movie">
+				<img src="imgs/cd-icon-location.svg" alt="Movie">
+			</div>
+			<!-- cd-timeline-img -->
+			<div class="cd-timeline-content">
+				<h2>Fatec Sorocaba</h2>
+				<p>Análise e Desenvolvimento de Sistemas</p>
+				<img class="img-timeline" src="imgs/fatec-sorocaba.jpg">
+				<a href="http://www.fatecsorocaba.edu.br/" target="_blank" class="cd-read-more">Conheça a instituição</a>
+				<span class="cd-date">Fevereiro 2016</span>
+			</div>
+			<!-- cd-timeline-content -->
+		</div>
+		<!-- cd-timeline-block -->
+		<div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-movie">
+				<img src="imgs/cd-icon-location.svg" alt="Picture">
+			</div>
+			<!-- cd-timeline-img -->
+			<div class="cd-timeline-content">
+				<h2>Essencial Contabilidade</h2>
+				<p>Estágio
+					<p>
+						<p>Funções: Conferir e importar notas fiscais, gerar declarações e organizar os arquivos.</p>
+						<a href="https://www.essencialcontabil.com.br/" target="_blank" class="cd-read-more">Conheça a empresa</a>
+						<span class="cd-date">Agosto 2016</span>
+			</div>
+			<!-- cd-timeline-content -->
+		</div>
+		<!-- cd-timeline-block -->
+		<div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-movie">
+				<img src="imgs/cd-icon-location.svg" alt="Location">
+			</div>
+			<!-- cd-timeline-img -->
+			<div class="cd-timeline-content">
+				<h2>Faculdades Integradas Rio Branco</h2>
+				<img class="img-timeline" src="imgs/riobranco.jpg">
+				<p>Sistemas de Informação, campus Granja Vianna.</p>
+
+				<a href="http://www.riobrancofac.edu.br/site/Default.aspx" target="_blank" class="cd-read-more">Conheça a instituição</a>
+				<span class="cd-date">Agosto 2016</span>
+			</div>
+			<!-- cd-timeline-content -->
+		</div>
+		<!-- cd-timeline-block -->
+		<div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-movie">
+				<img src="imgs/cd-icon-location.svg" alt="Location">
+			</div>
+			<!-- cd-timeline-img -->
+			<div class="cd-timeline-content">
+				<h2>Cursos de Desenvolvimento</h2>
+				<img class="img-timeline" src="imgs/udemy.png">
+				<p>Desenvolvimento em ASP.NET, HTML5, CSS3, JavaScript, JQuery, PHP, MySQL, MS-SQL, C#, Java.</p>
+				<a href="https://www.udemy.com/" target="_blank" class="cd-read-more">Conheça a instituição</a>
+				<span class="cd-date">Dezembro 2016</span>
+			</div>
+			<!-- cd-timeline-content -->
+		</div>
+		<!-- cd-timeline-block -->
+		<div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-picture">
+				<img src="imgs/cd-icon-location.svg" alt="Movie">
+			</div>
+			<!-- cd-timeline-img -->
+			<div class="cd-timeline-content">
+				<h2>Projetos</h2>
+				<p>Diversos sites e sistemas desenvolvidos por mim para testar os conhecimentos e propor soluções aos problemas que encontro.</p>
+				<a href="https://www.github.com/lucasmancan" target="_blank" class="cd-read-more">Conheça os projetos</a>
+				<span class="cd-date">2017-2018</span>
+			</div>
+			<!-- cd-timeline-content -->
+		</div>
+		<!-- cd-timeline-block -->
+	</section>
+	<!-- cd-timeline -->
+	<section id="servicos">
+		<div class="container">
+			<div class="row ">
+				<div class="col-md-8 anime">
+					<h1>Serviços</h1>
+					<p>Desenvolvimento de sites, aplicações web e marketing digital utilizando as melhores práticas e tecnologias de programação.</p>
+				</div>
+				<div class="col-md-4">
+					<img id="desenho" src="imgs/Desenho.png" alt="trabalhos">
+				</div>
+			</div>
+		</div>
+	</section>
+	<section id="contato">
+		<div class="container anime">
+			<h1>Contato</h1>
+			<form id="formulario" class="form-group" action="mail_handler.php" method="post">
+				<input class="form-control" type="text" name="first_name" placeholder="Nome para contato">
+				<br>
+				<input id="email" class="form-control" type="text" name="email" placeholder="Email">
+				<br>
+				<br>
+				<textarea class="form-control" rows="4" name="message" cols="20" placeholder="Mensagem que será enviada"></textarea>
+				<br>
+				<input class="btn-black" type="submit" name="submit" value="Enviar">
+			</form>
+		</div>
+	</section>
+	<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+	
+	
+	<script src="js/typed.js"></script>
+	<script src="js/main.js"></script>
+	<script src="js/modernizr.js"></script>
+	<script language="javascript" src="js/scripts.js"></script>
+	<footer id="footer">
+		<p>2017 &copy; Lucas Frederico Mançan</p>
+	</footer>
+</body>
+
+</html>
