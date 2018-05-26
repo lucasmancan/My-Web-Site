a402d3fc3064ead61c9ae59b868848f8c7557df3
Lucas Frederico Mançan <lucasfmancan@hotmail.com>
2018-05-26 18:21:54 -0300

    Making more professional

---
+++ thank_you.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contato | Lucas Mançan</title>
+    <link href="css/styles.min.css" rel= "stylesheet">
+</head>
+<body>
+<div id="particles-js"></div>
+<script src="js/particles.min.js"></script>
+<script src="js/app.min.js"></script>
+    <div class="capa">
+        <div class= "texto-capa">
+        <h1>Obrigado!</h1>
+        <h1>Entrarei em contato o mais rápido possível.</h1>
+        <a class= " btn-principal" href="index.php">Voltar</a>
+        </div>
+    </div>
+</body>
+</html>
